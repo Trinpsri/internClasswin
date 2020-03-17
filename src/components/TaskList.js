1c862cb6c3934729e0bc556e07c2ed06ccdd216d
Trin Pongsri <trinpsri11@gmail.com>
2020-03-17 18:07:10 +0700

    Add Cool Loading Task

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,11 +4,29 @@ import { action } from "@storybook/addon-actions";
 
 function TaskList({ tasks, loading, actions }) {
 
+    const loadingRow = (
+        <div className="loading-item">
+            <span className="glow-checkbox" />
+            <span className="glow-text">
+                <span>Loading</span>
+                <span>cool</span>
+                <span>state</span>
+            </span>
+        </div>
+    )
+
     if (loading) {
-        return <div className="list-items">loading</div>
+        return <div className="list-items">
+            {loadingRow}
+            {loadingRow}
+            {loadingRow}
+            {loadingRow}
+            {loadingRow}
+            {loadingRow}
+        </div>
     }
     if (tasks.Length === 0){
-        return <div className="list-items">No items...</div>
+        return <div className="list-items">No items</div>
     }
 
     return (
